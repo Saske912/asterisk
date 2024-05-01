f8dd783899003c48a76191614932cf3b2f331eec
Mihail Prigorodov <saveloy@yandex.ru>
2024-05-01 10:28:59 +0300

    tmp

---
+++ prisma/seed/trunk_dialpatterns.ts
@@ -0,0 +1,32 @@
+export const trunk_dialpatterns = {
+	"trunk_dialpatterns": [
+		{
+			"trunkid": 2,
+			"match_pattern_prefix": "",
+			"match_pattern_pass": "+7XXXXXXXXXX",
+			"prepend_digits": "",
+			"seq": 0
+		},
+		{
+			"trunkid": 2,
+			"match_pattern_prefix": "",
+			"match_pattern_pass": "8XXXXXXXXXX",
+			"prepend_digits": "",
+			"seq": 1
+		},
+		{
+			"trunkid": 2,
+			"match_pattern_prefix": "",
+			"match_pattern_pass": "XXXXXXX",
+			"prepend_digits": "",
+			"seq": 2
+		},
+		{
+			"trunkid": 2,
+			"match_pattern_prefix": "",
+			"match_pattern_pass": "XXXXXXXXXXX",
+			"prepend_digits": "+",
+			"seq": 3
+		}
+	]
+}
